97b8af47231d8a9b3ea0a331e8e1bcae406043f1
danil <danil.spb.it@gmail.com>
2021-08-17 23:45:56 +0300

    fix template

--- SellBuildKRD/SellBuildKRD/urls.py
+++ SellBuildKRD/SellBuildKRD/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import django
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -24,4 +25,5 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path("account/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
+    path('about', include('django.contrib.flatpages.urls'))
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
